a13499f4239f1aecaa5c18034486e39730e66198
Will Reed <wreed@programmer.net>
2025-03-07 21:19:59 -0600

    using leaves

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -5,67 +5,62 @@
   "temporary directory for emacs files")
 
 ;; use /tmp for backup and autosave
-(setq backup-directory-alist
-  `((".*" . ,emacs/tmp)))
-(setq auto-save-file-name-transforms              ;; use /tmp for auto-save files
-  `((".*" ,emacs/tmp t)))
-(setq auto-save-list-file-prefix emacs/tmp)
+(setopt backup-directory-alist `((".*" . ,emacs/tmp)))
+(setopt auto-save-file-name-transforms `((".*" ,emacs/tmp t)))
+(setopt auto-save-list-file-prefix emacs/tmp)
 
 ;; list of needed directories
 (setq-local needed-dirs
-  (list "local" "assets" "cache" "cache/transient" "cache/projectile"))
+  (list "cache" "cache/tree-sitter"
+        "cache/transient" "cache/projectile" "cache/eln"
+        "cache/packages" "cache/packages/gnupg"))
 
 ;; create the needed-dirs if they aren't there
 (dolist (dir needed-dirs)
-  (when (not (file-exists-p (concat user-emacs-directory dir)))
-    (mkdir (concat user-emacs-directory dir) t)))
+  (unless (file-exists-p (expand-file-name dir user-emacs-directory))
+    (mkdir (expand-file-name dir user-emacs-directory) t)))
 
-(setq native-compile-target-directory (concat user-emacs-directory "cache/eln/"))
-(startup-redirect-eln-cache native-compile-target-directory)
+(setopt load-prefer-newer t) ;; prefer newer '.el' files over older '.elc/.eln' files
+(setopt package-enable-at-startup nil)
 
-(setq load-prefer-newer t)
-(setq package-enable-at-startup nil)
+(when (native-comp-available-p)
+  (setopt native-compile-target-directory (concat user-emacs-directory "cache/eln/"))
+  (startup-redirect-eln-cache native-compile-target-directory)
+  (defconst native-comp-opts
+    '("-mtune=native" "-march=skylake" "-O3"))
+  (setopt native-comp-speed 3
+          native-comp-jit-compilation t
+          native-comp-async-warnings-errors-kind nil
+          native-comp-compiler-options native-comp-opts
+          native-comp-verbose 0
+          native-comp-enable-subr-trampolines t
+          native-comp-async-jobs-number 9))
 
-(defconst native-comp-opts
-  '("-mtune=native" "-march=skylake" "-O3"))
-
-(setq package-native-compile t
-      native-comp-speed 3
-      native-comp-jit-compilation t
-      native-comp-async-warnings-errors-kind nil
-      native-comp-compiler-options native-comp-opts
-      native-comp-verbose 0
-      native-comp-enable-subr-trampolines t
-      native-comp-async-jobs-number 9)
-
-(push '(background-color . "#1f1f1f") default-frame-alist)
-(push '(foreground-color . "#fffaaa") default-frame-alist)
+;; avoid the flashbang
+(push '(background-color . "#1D1F21") default-frame-alist)
+(push '(foreground-color . "#B294BB") default-frame-alist)
+;; disable ui elements I don't want
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . nil) default-frame-alist)
 (push '(horizontal-scroll-bars . nil) default-frame-alist)
 (push '(font . "JetBrains Mono-14") default-frame-alist)
-(tooltip-mode nil)
+;(tooltip-mode nil)
 
-(setq frame-title-format '("gnu emacs: %b"))
+(setopt frame-title-format '("gnu emacs: %b"))
 
-
+;; change default echo area message
 (advice-add 'startup-echo-area-message
-  :override #'(lambda () (message "loaded")))
+  :override #'(lambda () (message "What's GNU with you?")))
 
-(setq native-comp-async-report-warnings-errors 'silent)
-(setq warning-minimum-level 'error)
-(setq warning-minimum-log-level 'error)
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
+;; reduce error messaging
+(setq native-comp-async-warnings-errors-kind 'error
+      native-comp-async-report-warnings-errors nil
+      warning-suppress-log-types '((comp) (bytecomp))
+      byte-compile-warning-types '(not obsolete)
+      warning-minimum-log-level :error
+      warning-minimum-level :error)
 
-(add-to-list 'load-path
-  (concat user-emacs-directory "local"))
-
-;; i hate customize with a passion
-(setq custom-file
-  (concat user-emacs-directory "cache/custom.el"))
-
-(setq safe-local-variable-directories '("~/.config/emacs"))
-
-(load-library "definitions")
+;; allows me to ignore the customize interface entirely <3
+(setopt custom-file (concat user-emacs-directory "cache/custom.el"))
+(setopt safe-local-variable-directories '("~/.config/emacs"))
